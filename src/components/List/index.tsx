53682b38ab26e3cc202af8e96e3a9e02081908e6
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-27 20:41:43 -0300

    Controlling the form

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './List.module.scss';
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { task: 'React', time: '02:00:00' },
     { task: 'Javascript', time: '01:00:00' },
     { task: 'Typescript', time: '03:30:00' },
     { task: 'Express', time: '01:50:00' },
-  ];
+  ]);
   return (
     <aside className={style.taskList}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: 'Estudar Estado', time: '05:00' }]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
