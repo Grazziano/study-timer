b347f94ee648a4ca495fb091165d83583fd1ab91
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-27 11:15:43 -0300

    Creating form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Button from '../Button';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label htmlFor="task">Adicione um novo estudo</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            placeholder="O que você quer estudar"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="time">Tempo</label>
+          <input
+            type="time"
+            step="1"
+            name="time"
+            id="time"
+            min="00:00:00"
+            max="01:30:00"
+          />
+        </div>
+        <Button />
+      </form>
+    );
+  }
+}
