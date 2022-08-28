e28f52b9d3367cb0a44bf3d9571c0ce91d16b626
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-28 14:52:04 -0300

    Refactoring class components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from './Button.module.scss';
 
 interface IProps {
-  children: React.ReactChild;
+  children?: React.ReactNode;
   type?: 'button' | 'submit' | 'reset' | undefined;
   onClick?: () => void;
 }
 
-export default class Button extends Component<IProps> {
-  render() {
-    const { type = 'button', onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.button}>
-        {this.props.children}
-      </button>
-    );
-  }
+function Button({ onClick, type, children }: IProps) {
+  return (
+    <button onClick={onClick} type={type} className={style.button}>
+      {children}
+    </button>
+  );
 }
+
+// export default class Button extends Component<IProps> {
+//   render() {
+//     const { type = 'button', onClick } = this.props;
+//     return (
+//       <button onClick={onClick} type={type} className={style.button}>
+//         {this.props.children}
+//       </button>
+//     );
+//   }
+// }
+
+export default Button;
